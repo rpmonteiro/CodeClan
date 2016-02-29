b4836f115da5e6e9cbcf186d417cee576155d4d2
rpmonteiro <ric.p.monteiro@gmail.com>
2016-02-29 11:39:30 +0000

    more database files. Foreign keys and update the database

---
+++ week3/monday/databases/pet.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pet(
+  id SERIAL8,
+  name VARCHAR(255),
+  owner VARCHAR(255),
+  date_of_birth DATE,
+  date_of_death DATE
+);
--- week3/monday/databases/script.sql
+++ week3/monday/databases/script.sql
@@ -1,3 +1,5 @@
-INSERT INTO person (name, age, sex) VALUES ('marc', 28, 'M');
+INSERT INTO person (name, age, sex) VALUES ('valerie', 28, 'F');
 
-INSERT INTO person (name, age, sex) VALUES ('rick', 18, 'M');
+INSERT INTO person (name, age, sex) VALUES ('valerie', 23, 'F');
+
+INSERT INTO person (name, age, sex) VALUES ('john', 19, 'M');
