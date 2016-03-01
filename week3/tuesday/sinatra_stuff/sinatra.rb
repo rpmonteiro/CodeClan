a329cf8afe93a4e1e94c6a52a4bb9a532961d4c8
rpmonteiro <ricpmonteiro@gmail.com>
2016-03-01 10:44:54 +0000

    my first route! :)

--- week3/tuesday/sinatra_stuff/sinatra.rb
+++ week3/tuesday/sinatra_stuff/sinatra.rb
@@ -1,5 +1,18 @@
-require('sinatra')
+require 'sinatra'
+require 'sinatra/contrib/all' if development?
+
+get '/info' do
+  return 'info page.. yeahh'
+end
+
+get '/name' do
+  return 'oh hai!'
+end
+
+get '/address' do
+  return 'I live here.'
+end
 
 get '/hello' do
-   return 'hello world!'
+  return 'hello world!'
 end
