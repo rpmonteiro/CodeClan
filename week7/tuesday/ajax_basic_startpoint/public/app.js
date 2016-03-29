4271ef91834c2cd9a33ee1e30b6e86172a94b132
rpmonteiro <ric.p.monteiro@gmail.com>
2016-03-29 12:19:13 +0100

    my first api request! yay

--- week7/tuesday/ajax_basic_startpoint/public/app.js
+++ week7/tuesday/ajax_basic_startpoint/public/app.js
@@ -1,3 +1,16 @@
 window.onload = function(){
   console.log('App started');
+  var url = 'https://restcountries.eu/rest/v1/';
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onload = function() {
+    if (request.status === 200) {
+      console.log('got the data success');
+      // console.log(request.responseText);
+      var jsonString = request.responseText;
+      var countries = JSON.parse(jsonString);
+      console.log('countries', countries)
+    }
+  }
+  request.send();
 };
