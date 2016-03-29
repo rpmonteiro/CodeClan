c055d801f51689f40b4eb5a0ecf730dd2b6519be
rpmonteiro <ric.p.monteiro@gmail.com>
2016-03-29 10:57:29 +0100

    refactored the ugly code

--- week7/tuesday/local_storage_start_point/public/index.js
+++ week7/tuesday/local_storage_start_point/public/index.js
@@ -2,19 +2,26 @@ var init = function(){
   console.log('app started');
 
   var films = JSON.parse(localStorage.getItem('film_app_list')) || [];
-  console.log('on init got films:', films);
-
+  var ul = document.getElementById('film-list');
   var button = document.getElementById('add-button');
-  var handleClick = function(){
-    console.log('Woah I was got clicked')
-    var textInput = document.getElementById('film-text-input')
-    var filmName = textInput.value;
-    console.log('film name', filmName);
 
-    var li = document.createElement('li');
-    li.innerHTML = filmName;
-    var ul = document.getElementById('film-list');
-    ul.appendChild(li);
+  function insertItem(item) {
+    ul.insertAdjacentHTML('afterend', '<li>' + item + '</li>');
+  }
+
+  function grabTextInput() {
+    return document.getElementById('film-text-input').value;
+  }
+
+  films.forEach(function(filmName) {
+    insertItem(filmName);
+  })
+
+  var handleClick = function(){
+    var filmName = grabTextInput();
+    insertItem(filmName);
+    films.push(filmName);
+    localStorage.setItem('film_app_list', JSON.stringify(films));
   }
   button.onclick = handleClick;
 };
