b8076cec6be98952accf3c0e8e825d5eba863907
rpmonteiro <ric.p.monteiro@gmail.com>
2016-03-31 10:40:25 +0100

    Making charts in JS using highcharts

--- week7/wednesday/map_start_point/public/app.js
+++ week7/wednesday/map_start_point/public/app.js
@@ -2,19 +2,6 @@ function initialize() {
   var zoom = 14;
   var coordinates = {lat: 55.951998, lng: -3.18997};
   var map = new Map(coordinates, zoom);
-  map.addMarker(coordinates);
-  var currentLocation;
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      currentLocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      }
-      map.addMarker(currentLocation);
-    })
-  }
-
 }
 
 window.onload = initialize;
