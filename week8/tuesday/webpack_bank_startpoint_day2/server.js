e9ae9de6c2862ec999ff1a8bab75a32c3e7eca78
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-05 16:16:21 +0100

    started a mongodb database and established a connection

--- week8/tuesday/webpack_bank_startpoint_day2/server.js
+++ week8/tuesday/webpack_bank_startpoint_day2/server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
-var path = require('path')
+var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+})
+
+app.get('/accounts', function(req, res) {
+  var url = 'mongodb://localhost:27017/bank';
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    var collection = db.collection('accounts');
+  })
+})
 
 app.use(express.static('client/build'));
 
