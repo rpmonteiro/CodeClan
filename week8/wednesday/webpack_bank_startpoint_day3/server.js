4fcf863b3836987e8d4121558945ad046321bd05
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-06 10:38:15 +0100

    added a post route and inserted data into db

--- week8/wednesday/webpack_bank_startpoint_day3/server.js
+++ week8/wednesday/webpack_bank_startpoint_day3/server.js
@@ -2,16 +2,19 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var url = 'mongodb://localhost:27017/bank';
+
 // var fs = require('fs');
 // var ACCOUNTS_FILE = path.join(__dirname + '/sample.json');
 
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.get('/accounts', function(req,res){
-  var url = 'mongodb://localhost:27017/bank';
+app.get('/accounts', function(req, res){
   MongoClient.connect(url, function(err, db){
     if(err){
       console.log(err);
@@ -25,9 +28,27 @@ app.get('/accounts', function(req,res){
   })
 });
 
+app.post('/accounts', function(req, res) {
+  console.log('body', req.body);
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    var collection = db.collection('accounts');
+    collection.insert({
+      "owner": req.body.owner,
+      "amount": req.body.amount,
+      "type": req.body.type
+    })
+    res.status(200).end();
+    db.close();
+  });
+})
+
 app.use(express.static('client/build'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
