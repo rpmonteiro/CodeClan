ef2ccbaa7ed42ccbf2bde5dead7ad02f557a2c53
rpmonteiro <ric.p.monteiro@gmail.com>
2016-04-06 09:39:36 +0100

    started the backend for a full stack application;

--- week8/wednesday/webpack_bank_startpoint_day3/server.js
+++ week8/wednesday/webpack_bank_startpoint_day3/server.js
@@ -2,28 +2,30 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
+// var fs = require('fs');
+// var ACCOUNTS_FILE = path.join(__dirname + '/sample.json');
 
-app.get('/', function(req, res) {
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-})
+});
 
-app.get('/accounts', function(req, res) {
+app.get('/accounts', function(req,res){
   var url = 'mongodb://localhost:27017/bank';
-  MongoClient.connect(url, function(err, db) {
-    if (err) {
+  MongoClient.connect(url, function(err, db){
+    if(err){
       console.log(err);
       return;
     }
     var collection = db.collection('accounts');
-    collection.find({}).toArray(function(err, docs) {
+    collection.find({}).toArray(function(err, docs){
       res.json(docs);
       db.close();
-    });
+    })
   })
 });
 
 app.use(express.static('client/build'));
-
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
