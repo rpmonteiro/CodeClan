3a93b47034f101488fb8ef631a0bcabdfd5a4a2e
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-12 12:04:16 +0100

    added the dependencies needed for the router on the index.js file

--- week13/thursday/react_router/client/src/app.js
+++ week13/thursday/react_router/client/src/app.js
@@ -2,6 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Main = require('./components/Main.jsx');
 
+var About = require('./components/About');
+var Home = require('./components/Home');
+var Pricing = require('./components/Pricing');
+
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+var hashHistory = require('react-router').hashHistory;
+
 window.onload = function(){
   ReactDOM.render(
     <Main></Main>,
