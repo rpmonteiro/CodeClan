b62542aed7e0892aed7bc1ade498aca7ee553108
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-16 14:10:23 +0100

    ran rake:db migrate to actually create the table in the database

--- wk14/monday/animals/db/schema.rb
+++ wk14/monday/animals/db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516121457) do
+ActiveRecord::Schema.define(version: 20160516130905) do
 
   create_table "animals", force: :cascade do |t|
+    t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plants", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
