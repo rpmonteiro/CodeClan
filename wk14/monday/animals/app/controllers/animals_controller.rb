ad9eb45ec73f2e18e4c415b1370307ef1c4134ed
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-16 11:19:57 +0100

    render a view using json

--- wk14/monday/animals/app/controllers/animals_controller.rb
+++ wk14/monday/animals/app/controllers/animals_controller.rb
@@ -1,5 +1,11 @@
+# no doc
 class AnimalsController < ApplicationController
   def index
-
+    @animals = [{ name: 'Tiger' }, { name: 'Snow Leopard' }]
+    # render :json => @animals Renders json automatically. no more html
+    respond_to do |format|
+      format.html
+      format.json { render :json => @animals }
+    end
   end
 end
