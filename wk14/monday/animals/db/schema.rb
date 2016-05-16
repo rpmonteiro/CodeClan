f6ef1e6ad217a5325f4f6db0b7b453c85c0b06ef
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-16 14:51:42 +0100

    commited the changes to the actual database by running rake db:migrate

--- wk14/monday/animals/db/schema.rb
+++ wk14/monday/animals/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516130905) do
+ActiveRecord::Schema.define(version: 20160516134721) do
 
   create_table "animals", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "legs"
   end
 
   create_table "plants", force: :cascade do |t|
