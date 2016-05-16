2a6dd7ee1165f883370a5e8931d42f73d6cc7abc
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-16 13:47:27 +0100

    created a database and made an animal in the rails console

--- wk14/monday/animals/app/controllers/animals_controller.rb
+++ wk14/monday/animals/app/controllers/animals_controller.rb
@@ -1,7 +1,7 @@
 # no doc
 class AnimalsController < ApplicationController
   def index
-    @animals = [{ name: 'Tiger' }, { name: 'Snow Leopard' }]
+    # @animals = [{ name: 'Tiger' }, { name: 'Snow Leopard' }]
     # render :json => @animals Renders json automatically. no more html
     respond_to do |format|
       format.html
