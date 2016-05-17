9bb9adb68d6111fa90178abe321b396c9359d930
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-17 11:43:19 +0100

    setup a has_many through relationship. It creates an inner join.

--- wk14/tuesday/ticketmaster/app/models/artist.rb
+++ wk14/tuesday/ticketmaster/app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :albums, dependent: :destroy
   has_many :gigs
+  # so we can lookup Artist.Venue
+  # same as has_many( :venues, { through: :gigs } )
+  has_many :venues, through: :gigs
 end
