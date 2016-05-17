f0a54b196960a008e758669bada2089c70d59c37
rpmonteiro <ric.p.monteiro@gmail.com>
2016-05-17 10:22:06 +0100

    added the has_many relationship in the artist model. rails dark magic.

--- wk14/tuesday/ticketmaster/db/schema.rb
+++ wk14/tuesday/ticketmaster/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517084849) do
+ActiveRecord::Schema.define(version: 20160517090804) do
+
+  create_table "albums", force: :cascade do |t|
+    t.text     "name"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "albums", ["artist_id"], name: "index_albums_on_artist_id"
 
   create_table "artists", force: :cascade do |t|
     t.text     "name"
